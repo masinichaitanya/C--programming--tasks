89d29928c8748bcde745ae43c25aa466144fc04c
masinichaitanya <57840991+masinichaitanya@users.noreply.github.com>
2019-11-16 22:44:15 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	char a[10],c[10];
+	int b;
+	printf("Enter your name:");
+	scanf("%s",&a);
+	printf("\nEnter your age:");
+	scanf("%d",&b);
+	printf("\nEnter your phone number:");
+	scanf("%s",&c);
+	printf("\nDETAILS");
+	printf("\n\tName:%s",a);
+	printf("\n\tAge:%d",b);
+	printf("\n\tPhone number:%s",c);
+}
+	 
+
